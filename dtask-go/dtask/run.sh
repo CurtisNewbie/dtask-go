91dd8ce2b56b9581cb9fa2b28bd93d3ac27e27a6
yongj.zhuang <zhuangyongj@gmail.com>
2022-10-26 01:11:10 +0800

    Modified: dtask-go/dtask/run.sh

--- dtask-go/dtask/run.sh
+++ dtask-go/dtask/run.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-./main profile='prod' configFile=/usr/src/dtaskgo/config/app-conf-prod.json >> /usr/src/dtaskgo/logs/dtaskgo.log 2>&1
+pid=0
+logfile="/usr/src/dtaskgo/logs/dtaskgo.log"
+if [ ! -f "$logfile" ]; then
+    touch "$logfile"
+fi
 
+trap 'kill ${!}; kill -SIGTERM "$pid"' SIGTERM
+
+./main profile='prod' configFile=/usr/src/dtaskgo/config/app-conf-prod.json >> "$logfile" 2>&1
+
+pid="$!"
+
+# wait forever
+while true
+do
+  tail -f /dev/null & wait ${!}
+done
