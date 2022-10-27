059cd29d5b733573d34b748ddb448e7cb70b60b0
yongj.zhuang <zhuangyongj@gmail.com>
2022-10-27 23:39:35 +0800

    Modified: dtask-go/deploy.sh Modified: dtask-go/dtask/app-conf-prod.json

--- dtask-go/deploy.sh
+++ dtask-go/deploy.sh
@@ -2,7 +2,8 @@
 
 # --------- remote
 remote="alphaboi@curtisnewbie.com"
-remote_path="~/services/dtaskgo/build"
+remote_path="~/services/dtaskgo/build/"
+remote_config_path="~/services/dtaskgo/config/"
 # ---------
 
 #scp "run.sh" "${remote}:${remote_path}"
@@ -11,9 +12,14 @@ remote_path="~/services/dtaskgo/build"
 # copy the config file just in case we updated it
 # scp "app-conf-dev.json" "${remote}:${remote_path}"
 
-scp -r dtask/* "${remote}:${remote_path}/"
+scp -r dtask/* "${remote}:${remote_path}"
+if [ ! $? -eq 0 ]; then
+    exit -1
+fi
+scp dtask/app-conf-prod.json "${remote}:${remote_config_path}"
 if [ ! $? -eq 0 ]; then
     exit -1
 fi
 
+ssh  "alphaboi@curtisnewbie.com" "mv ~/services/dtaskgo/logs/dtaskgo.log ~/services/dtaskgo/logs/dtaskgo-$(date +'%Y%m%d_%H%M%S').log"
 ssh  "alphaboi@curtisnewbie.com" "cd services; docker-compose up -d --build dtaskgo"
